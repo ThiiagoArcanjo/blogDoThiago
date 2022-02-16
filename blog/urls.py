5d78602df17e34c169720b7020e35837c7e951f2
Thiago Arcanjo <thiagokai.arcanjo@gmail.com>
2022-02-16 16:44:23 -0300

    Adicionando formulario

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.lista_post, name='lista_post'),
     path('post/<int:pk>/', views.post_detalhes, name='post_detalhes'),
+    path('post/novo/', views.novo_post, name='novo_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 
@@ -11,3 +12,7 @@ def lista_post(request):
 def post_detalhes(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detalhes.html', {'post': post})
+
+def novo_post(request):
+    form  = PostForm()
+    return render(request, 'blog/post_edite.html', {'form':form})
