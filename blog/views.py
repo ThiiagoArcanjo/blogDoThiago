e9e35d1b8ca34242e63095d88a1a59844c278351
Thiago Arcanjo <thiagokai.arcanjo@gmail.com>
2022-02-16 16:54:52 -0300

    Adicinando inserção na view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+
 
 
 
@@ -14,5 +15,16 @@ def post_detalhes(request, pk):
     return render(request, 'blog/post_detalhes.html', {'post': post})
 
 def novo_post(request):
-    form  = PostForm()
-    return render(request, 'blog/post_edite.html', {'form':form})
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.data_de_publicacao = timezone.now()
+            post.save()
+            return redirect('post_detalhes', pk=post.pk)
+    
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_edite.html', {'form': form}) 
