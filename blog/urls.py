833d0103ffc36a2067e8235b8f0f3bfb03d11078
Thiago Arcanjo <thiagokai.arcanjo@gmail.com>
2022-02-16 15:10:10 -0300

    Adicionando link de post de detalhes, url, view e html

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_post, name='lista_post'),
+    path('post/<int:pk>/', views.post_detalhes, name='post_detalhes'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
 
 
 def lista_post(request):
     posts = Post.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('data_de_publicacao')
     return render(request, 'blog/lista_post.html', {'posts': posts})
+
+def post_detalhes(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detalhes.html', {'post': post})
