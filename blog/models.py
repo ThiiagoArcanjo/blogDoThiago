664ece4591a314d83a5582fbf91fd6299d3dd92e
Thiago Arcanjo <thiagokai.arcanjo@gmail.com>
2022-02-16 00:13:40 -0300

    Adicionando mudanças ao modelo

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     titulo = models.CharField(max_length=200)
     texto = models.TextField()
     data_de_criacao = models.DateTimeField(default=timezone.now)
-    data_de_Publicacao = models.DateTimeField(blank=True, null=True)
+    data_de_publicacao = models.DateTimeField(blank=True, null=True)
 
     def publicar(self):
         self.data_de_Publicacao = timezone.now()
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 def lista_post(request):
-    return render(request, 'blog/lista_post.html', {})
+    posts = Post.objects.filter(data_de_publicacao__lte=timezone.now()).order_by('data_de_publicacao')
+    return render(request, 'blog/lista_post.html', {'posts': posts})
